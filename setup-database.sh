ef44b673f45e66325b7927521ff6398383e3ed29
hendisantika <hendisantika@yahoo.co.id>
2019-03-11 04:34:09 +0700

    #34 - Add setup-database.sh file

---
+++ setup-database.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+service mysql start
+
+dbname=belajar
+dbusername=belajar
+dbpassword=java
+
+if [ "$MYSQL_DATABASE" ]; then
+    dbname="$MYSQL_DATABASE"
+fi
+
+
+if [ "$MYSQL_USERNAME" ]; then
+    dbusername="$MYSQL_USERNAME"
+fi
+
+if [ "$MYSQL_PASSWORD" ]; then
+    dbpassword="$MYSQL_PASSWORD"
+fi
+
+echo "GRANT ALL ON \`$dbname\`.* to '"$dbusername"'@'localhost' IDENTIFIED BY '"$dbpassword"'" | mysql -uroot -p
+echo "CREATE DATABASE IF NOT EXISTS \`$dbname\`" | mysql -uroot -p
+
+exec "$@"
+
