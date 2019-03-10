b2705a90a82176f77f273c8d046c0b304c590a7b
hendisantika <hendisantika@yahoo.co.id>
2019-03-11 03:56:59 +0700

    #27 - Add ProductRepositoryTest

---
+++ src/test/java/com/hendisantika/dockerworkflow4springbootdeveloper/repository/ProductRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.hendisantika.dockerworkflow4springbootdeveloper.repository;
+
+import com.hendisantika.dockerworkflow4springbootdeveloper.entity.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : docker-workflow-4-springboot-developer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-03-11
+ * Time: 03:55
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+@Sql(scripts = {"/mysql/delete-data.sql", "/mysql/sample-product.sql"})
+public class ProductRepositoryTest {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    public void testSave() {
+        Product p = new Product();
+        p.setCode("T-001");
+        p.setName("Test Product 001");
+        p.setPrice(new BigDecimal("100000.01"));
+
+        Assert.assertNull(p.getId());
+        productRepository.save(p);
+        Assert.assertNotNull(p.getId());
+    }
+
+    @Test
+    public void testFindById() {
+        Optional<Product> p = productRepository.findById("abc123");
+        Assert.assertNotNull(p);
+        Assert.assertEquals("P-001", p.get().getCode());
+        Assert.assertEquals("Product 001", p.get().getName());
+        Assert.assertEquals(BigDecimal.valueOf(101000.01), p.get().getPrice());
+
+        Assert.assertNull(productRepository.findById("notexist"));
+    }
+}
