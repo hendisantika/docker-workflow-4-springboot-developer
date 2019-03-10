e74a09a349b107d73c7fcac6fccc764b816715ef
hendisantika <hendisantika@yahoo.co.id>
2019-03-11 03:40:14 +0700

    #14 - Enable JDBC Http Session

--- src/main/java/com/hendisantika/dockerworkflow4springbootdeveloper/DockerWorkflow4SpringbootDeveloperApplication.java
+++ src/main/java/com/hendisantika/dockerworkflow4springbootdeveloper/DockerWorkflow4SpringbootDeveloperApplication.java
@@ -1,13 +1,34 @@
 package com.hendisantika.dockerworkflow4springbootdeveloper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+import java.io.File;
+import java.nio.file.Files;
 
 @SpringBootApplication
-public class DockerWorkflow4SpringbootDeveloperApplication {
+@EnableJdbcHttpSession
+public class DockerWorkflow4SpringbootDeveloperApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(DockerWorkflow4SpringbootDeveloperApplication.class, args);
+    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DockerWorkflow4SpringbootDeveloperApplication.class);
+
+    @Value("${upload.location}")
+    private String uploadLocation;
+
+    @Override
+    public void run(String... args) throws Exception {
+        File uploadFolder = new File(uploadLocation);
+        LOGGER.info("Creating upload folder [{}] at [{}]", uploadFolder.toPath(), uploadFolder.getAbsolutePath());
+        Files.createDirectories(uploadFolder.toPath());
     }
 
 }
